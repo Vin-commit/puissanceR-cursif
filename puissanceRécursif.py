b5627816549c4ae5e622b64d758eb51591bb088c
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-16 19:18:00 +0100

    Add files via upload

---
+++ puissanceRécursif.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/python3
+#coding: utf-8
+
+
+def power(x, y):
+  if y == 0:
+        return 1
+  else:
+        return x * power(x, y-1)
+
+
+try:
+  x = int(input("Quel est le nombre de base : "))
+  y = int(input("Quelle est la puissance : "))
+  print (x, "^", y, "=", power(x,y))
+except:
+  print(“Veuillez saisir un nombre.”)
+
+
+--------------------------------------------------------------------- Résultat --------------------------------------------------------------------------------
+Quel est le nombre de base : 10
+Quelle est la puissance : 3
+10 ^ 3 = 1000
